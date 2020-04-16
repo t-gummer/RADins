f3a0c5842444527c93628500769e4dc9d681fd2d
Timothy Gummer <Timothy.Gummer@Finity.local>
2020-04-17 00:20:14 +1000

    Add dialogue box

--- R/functions_change_shortcuts.R
+++ R/functions_change_shortcuts.R
@@ -1,7 +1,11 @@
+# NOTE: ----
+# Map the add-ins to shortcuts.
+
+# CODE: ----
 
 #' Set keyboard shortcuts
 #'
-#' @description Manipulated the JSON file to make the suggested keyboard shortcuts. Need to restart RStudio for them to take effect.
+#' @description Manipulated the JSON file to make the suggested keyboard shortcuts. Need to close and re-open RStudio (or start a new session) for them to take effect.
 #'
 #' @param addins Characer vector of underlying functions for addins. Need to be in the right form (e.g. \code{"fwdSlash::copy_file_path"}).
 #' @param shortcuts Characer vector of keyboard shortcuts for addins. Need to be in the right form (e.g. \code{"Ctrl+Shift+V"}).
@@ -13,15 +17,20 @@ NULL
 #' @name set_keyboard_shortcuts
 #' @export
 set_shortcuts <- function(addins, shortcuts){
-  addins.json <- jsonlite::read_json(path = "~/.R/rstudio/keybindings/addins.json")
+  json_path <- "~/.R/rstudio/keybindings/addins.json"
+  if(file.exists(json_path)){
+    addins.json <- jsonlite::read_json(path = json_path)
+  }else{
+    addins.json <- list()
+  }
   for(ii in seq_along(shortcuts)){
     addins.json[[addins[ii]]] <- shortcuts[ii]
   }
   jsonlite::write_json(addins.json,path = "~/.R/rstudio/keybindings/addins.json")
+  rstudioapi::showDialog(title = "Clipboard unavailable","Add-in shortcuts changed using <em>fwdSlash</em>.<p><strong>Close and re-open R Studio for this to take effect.</strong></p>")
 }
 
 #' @name set_keyboard_shortcuts
-#' @export
 set_shortcuts_default <- function(){
   # pkg <- "fwdSlash"
   # addins <- c("copy_file_path","paste_file_path","paste_vector_code","hard_code_vector_code","copy_r_eval")
@@ -42,3 +51,4 @@ set_shortcuts_default <- function(){
     )
   set_shortcuts(addins,shortcuts)
 }
+
